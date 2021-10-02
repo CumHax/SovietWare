ef822695590ee0c183283f8a72cf7e07db2fb48e
CumHax <83083809+CumHax@users.noreply.github.com>
2021-10-02 17:21:49 +0300

    Add files via upload

---
+++ src/main/java/me/cumhax/soviet/mixin/mixins/accessor/IMinecraft.java
@@ -0,0 +1,7 @@
+package me.cumhax.soviet.mixin.mixins.accessor;
+
+import net.minecraft.util.Timer;
+
+public interface IMinecraft {
+   Timer getTimer();
+}
---
+++ src/main/java/me/cumhax/soviet/mixin/mixins/accessor/ITimer.java
@@ -0,0 +1,7 @@
+package me.cumhax.soviet.mixin.mixins.accessor;
+
+public interface ITimer {
+   float getTickLength();
+
+   void setTickLength(float var1);
+}
